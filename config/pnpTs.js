69beb6874e9f1360945e25339d3675b6952cd4d6
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-06-17 01:02:57 +0530

    Create basic CRA config

--- config/pnpTs.js
+++ config/pnpTs.js
@@ -1,1 +1,35 @@
-module.exports = require('react-scripts/config/pnpTs');
+'use strict';
+
+const { resolveModuleName } = require('ts-pnp');
+
+exports.resolveModuleName = (
+  typescript,
+  moduleName,
+  containingFile,
+  compilerOptions,
+  resolutionHost
+) => {
+  return resolveModuleName(
+    moduleName,
+    containingFile,
+    compilerOptions,
+    resolutionHost,
+    typescript.resolveModuleName
+  );
+};
+
+exports.resolveTypeReferenceDirective = (
+  typescript,
+  moduleName,
+  containingFile,
+  compilerOptions,
+  resolutionHost
+) => {
+  return resolveModuleName(
+    moduleName,
+    containingFile,
+    compilerOptions,
+    resolutionHost,
+    typescript.resolveTypeReferenceDirective
+  );
+};
---
+++ config/utils.js
@@ -0,0 +1,28 @@
+/**
+ * Gets CLI args as object.
+ */
+export function getCommandArgs() {
+	const argList = process.argv;
+
+	const arg = {};
+	let a, opt, thisOpt, curOpt;
+	for (a = 0; a < argList.length; a++) {
+		thisOpt = argList[a].trim();
+		opt = thisOpt.replace(/^-+/, '');
+
+		if (opt === thisOpt) {
+			// argument value
+			if (curOpt) {
+				arg[curOpt] = opt;
+			}
+			curOpt = null;
+		} else {
+			// argument name
+			curOpt = opt;
+			arg[curOpt] = true;
+		}
+	}
+	return arg;
+};
+
+
