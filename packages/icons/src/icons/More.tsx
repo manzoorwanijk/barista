eae2e23d99a066ce1566930b331eafcfc61d1450
Vadim Nicolai <vadim@eventespresso.com>
2020-06-24 14:35:21 +0300

    More icon update. (#28)

--- packages/icons/src/icons/More.tsx
+++ packages/icons/src/icons/More.tsx
@@ -2,11 +2,8 @@ import * as React from 'react';
 
 function SvgMore(props: React.SVGProps<SVGSVGElement>) {
 	return (
-		<svg className='more_svg__icon' width={200} height={200} viewBox='0 0 1024 1024' {...props}>
-			<path
-				fill='#333'
-				d='M456 231a56 56 0 10112 0 56 56 0 10-112 0zm0 280a56 56 0 10112 0 56 56 0 10-112 0zm0 280a56 56 0 10112 0 56 56 0 10-112 0z'
-			/>
+		<svg aria-hidden='true' className='more_svg__ee-svg' height={20} width={20} {...props}>
+			<path d='M17 7V5H3v2h14zm0 4V9H3v2h14zm0 4v-2H3v2h14z' />
 		</svg>
 	);
 }
