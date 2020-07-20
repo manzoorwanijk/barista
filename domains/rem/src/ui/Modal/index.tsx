fe4295cab53b7432f21e82455938c750a4ac584f
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-07-21 00:25:56 +0530

    Remove convert date button

--- domains/rem/src/ui/Modal/index.tsx
+++ domains/rem/src/ui/Modal/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import classNames from 'classnames';
-import { useDisclosure } from '@chakra-ui/hooks';
 import { __ } from '@wordpress/i18n';
 
 import { ModalWithAlert } from '@eventespresso/components';
@@ -17,26 +15,21 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
-	const { isOpen: isRecurrenceOpen, onOpen: onRecurrenceOpen } = useDisclosure({ defaultIsOpen: false });
 	const cancelButtonProps = useCancelButtonProps(onClose);
-
-	const bodyClassName = classNames(!isRecurrenceOpen && 'ee-rem-modal__body--centered', 'ee-rem-modal__body');
-
-	const footerContent = isRecurrenceOpen && <ContentFooter />;
 
 	return (
 		<ModalWithAlert
-			bodyClassName={bodyClassName}
+			bodyClassName='ee-rem-modal__body'
 			cancelButtonProps={cancelButtonProps}
 			className='ee-rem-modal'
-			footerContent={footerContent}
+			footerContent={<ContentFooter />}
 			isOpen={isOpen}
 			onClose={onClose}
 			showAlertOnEscape={false}
 			title={__('Recurring Events Manager')}
 			withBorder
 		>
-			<ContentBody isRecurrenceOpen={isRecurrenceOpen} onRecurrenceOpen={onRecurrenceOpen} />
+			<ContentBody />
 		</ModalWithAlert>
 	);
 };
