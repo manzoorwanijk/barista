6fa46cbea3be2fad0ca31ccc8fa65482d8ccb354
Brent Christensen <brent@eventespresso.com>
2020-08-06 10:56:38 -0700

    refactor main file; load Barista on EE hook

--- ee-barista.php
+++ ee-barista.php
@@ -2,30 +2,36 @@
 /**
  * The plugin main file.
  *
- * @package         Event Espresso Barista
- * @author          Seth Shoultes
+ * @package 	Event Espresso Barista
+ * @author 		Seth Shoultes
  * @copyright   (c) 2008-2018 Event Espresso  All Rights Reserved.
- * @license         {@link http://eventespresso.com/support/terms-conditions/}
- * @see             Plugin Licensing
- * @link            {@link http://www.eventespresso.com}
+ * @license 	{@link http://eventespresso.com/support/terms-conditions/}
+ * @see 		Plugin Licensing
+ * @link 		{@link http://www.eventespresso.com}
  * Plugin Name: Event Espresso Barista
- * Plugin URI: http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
- * Description: Plugin useg for Event Espresso Packages development
+ * Plugin URI: http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign
+ * =wordpress_plugins_page&utm_content=support_link
+ * Description: Serving up fresh brewed Event Espresso Packages for your development pleasure
  * Version: 0.0.1
  * Author: Event Espresso
- * Author URI: http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
+ * Author URI: http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign
+ * =wordpress_plugins_page&utm_content=support_link
  * License: GPLv3
  * Text Domain: event_espresso
  * GitHub Plugin URI: https://github.com/eventespresso/packages
  */
-if (!defined('ABSPATH')) {
-    exit;
-}
+
+defined('ABSPATH') || die();
 
 define('EE_BARISTA_BASENAME', plugin_basename(__FILE__));
-
 define('EE_BARISTA_DIR', trailingslashit(plugin_dir_path(__FILE__)));
-
 define('EE_BARISTA_URL', trailingslashit(plugins_url('', __FILE__)));
 
-require_once __DIR__ . '/lib/load.php';
+add_action(
+	'AHEE__EE_System__load_espresso_addons',
+	function() {
+		require_once __DIR__ . '/lib/Barista.php';
+		$barista = new Barista();
+		$barista->initialize();
+	}
+);
