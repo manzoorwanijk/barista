2768b250846c2f494ef657be7372aeb50c7c4012
Brent Christensen <brent@eventespresso.com>
2020-09-01 11:25:00 -0700

    Fix duplicate modal buttons (#236)

--- packages/components/src/Modal/useSubmitButtonProps.tsx
+++ packages/components/src/Modal/useSubmitButtonProps.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { __ } from '@wordpress/i18n';
 
 import { SaveOutlined } from '@eventespresso/icons';
@@ -6,25 +6,18 @@ import { SaveOutlined } from '@eventespresso/icons';
 import { ButtonType } from '../../';
 import type { ButtonProps } from '../../';
 
-const useSubmitButtonProps = (onCloseModal: VoidFunction): ButtonProps => {
-	const onSubmit: ButtonProps['onClick'] = useCallback(
-		(e) => {
-			e.preventDefault();
-			onCloseModal();
-		},
-		[onCloseModal]
-	);
-
-	return useMemo<ButtonProps>(
-		() => ({
-			buttonText: __('Submit'),
-			buttonType: ButtonType.PRIMARY,
-			icon: SaveOutlined,
-			onClick: onSubmit,
-			type: 'submit',
-		}),
-		[onSubmit]
-	);
+const useSubmitButtonProps = (onSubmit: VoidFunction): ButtonProps => {
+	return useMemo<ButtonProps>(() => {
+		return onSubmit
+			? {
+					buttonText: __('Submit'),
+					buttonType: ButtonType.PRIMARY,
+					icon: SaveOutlined,
+					onClick: onSubmit,
+					type: 'submit',
+			  }
+			: null;
+	}, [onSubmit]);
 };
 
 export default useSubmitButtonProps;
