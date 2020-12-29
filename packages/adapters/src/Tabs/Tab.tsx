a42bc27c020a79907cd652d24408c7b155b94471
Brent Christensen <brent@eventespresso.com>
2020-12-29 04:40:41 -0800

    Improve Rich Text Editor Styles (#560)
    * set tabIndex
    
    * make styles more betterer
    
    * Small improvement
    
    Co-authored-by: Vadim Nicolai <nicolai.vadim@gmail.com>

--- packages/adapters/src/Tabs/Tab.tsx
+++ packages/adapters/src/Tabs/Tab.tsx
@@ -3,5 +3,5 @@ import { Tab as ChakraTab } from '@chakra-ui/react';
 import type { TabProps } from './types';
 
 export const Tab: React.FC<TabProps> = (props) => {
-	return <ChakraTab {...props} />;
+	return <ChakraTab tabIndex={0} {...props} />;
 };
