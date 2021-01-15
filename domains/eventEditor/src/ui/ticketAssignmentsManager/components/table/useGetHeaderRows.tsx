07081a5fabcc3911cd7e537466a103674ca540e9
Brent Christensen <brent@eventespresso.com>
2021-01-15 08:05:30 -0800

    add heading and help text to header (#622)

--- domains/eventEditor/src/ui/ticketAssignmentsManager/components/table/useGetHeaderRows.tsx
+++ domains/eventEditor/src/ui/ticketAssignmentsManager/components/table/useGetHeaderRows.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 
 import type { CellData, HeaderRow } from '@eventespresso/ui-components';
 import type { Ticket } from '@eventespresso/edtr-services';
+import { __ } from '@eventespresso/i18n';
 
 import HeaderCell from './HeaderCell';
 import useColClassName from './useColClassName';
@@ -9,7 +10,14 @@ import useColClassName from './useColClassName';
 const emptyCell: CellData = {
 	key: 'empty',
 	size: 'huge',
-	value: '',
+	value: (
+		<div>
+			<h3>{__('Assignments')}</h3>
+			<p>{__('Event Dates are listed below')}</p>
+			<p>{__('Tickets are listed along the top')}</p>
+			<p>{__('Click the cell buttons to toggle assigments')}</p>
+		</div>
+	),
 };
 
 const useGetHeaderRows = (tickets: Ticket[]): HeaderRow[] => {
