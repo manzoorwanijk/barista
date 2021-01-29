8b5474371048ab8cc396a88ea6e16686d6e01ac3
Vadim Nicolai <vadim@eventespresso.com>
2021-01-29 14:59:19 +0200

    Small improvement. (#686)

--- packages/ui-components/src/Checkbox/Checkbox.tsx
+++ packages/ui-components/src/Checkbox/Checkbox.tsx
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import { Checkbox as CheckboxAdapter } from '@eventespresso/adapters';
 import type { CheckboxProps } from '@eventespresso/adapters';
 
+import './style.scss';
+
 export const Checkbox: React.FC<CheckboxProps> = (props) => {
 	const className = classNames('ee-checkbox', props.className);
 
