a85a9984f925a18901991ebffe5bb0655e3f6028
Brent Christensen <brent@eventespresso.com>
2021-09-13 11:26:10 -0700

    A Few More UI Fixes (#988)

--- packages/icons/src/icons/Edit.tsx
+++ packages/icons/src/icons/Edit.tsx
@@ -13,7 +13,7 @@ const Edit = (props: IconProps): JSX.Element => {
 			className='ee-svg--edit'
 			{...props}
 		>
-			<g fill='none' stroke='currentColor' strokeWidth={2}>
+			<g fill='none' fillOpacity='0' stroke='currentColor' strokeWidth={2}>
 				<path d='M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7' />
 				<path d='M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z' />
 			</g>
