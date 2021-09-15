d9cff9d43ea66e07bba0d776d04a08a0207e1f29
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-09-15 22:23:59 +0530

    ✨ Form Builder | Add UI for Applies to field for sections (#1000)

--- packages/form-builder/src/FormSection/Tabs/Settings.tsx
+++ packages/form-builder/src/FormSection/Tabs/Settings.tsx
@@ -1,9 +1,28 @@
 import { __ } from '@eventespresso/i18n';
-import { TextInputWithLabel, SwitchWithLabel } from '@eventespresso/ui-components';
+import { TextInputWithLabel, SwitchWithLabel, SelectWithLabel } from '@eventespresso/ui-components';
 
 import { useUpdateSection } from '../useUpdateSection';
 
 import type { FormSectionProps } from '../../types';
+
+const APPLIES_TO_OPTIONS = [
+	{
+		value: 'ALL',
+		label: __('all'),
+	},
+	{
+		value: 'PRIMARY',
+		label: __('primary registrant'),
+	},
+	{
+		value: 'PURCHASER',
+		label: __('purchaser'),
+	},
+	{
+		value: 'REGISTRANTS',
+		label: __('registrants'),
+	},
+];
 
 export const Settings: React.FC<FormSectionProps> = ({ formSection }) => {
 	const onChangeValue = useUpdateSection(formSection);
@@ -24,6 +43,12 @@ export const Settings: React.FC<FormSectionProps> = ({ formSection }) => {
 				label={__('show label')}
 				onChangeValue={onChangeValue('label.showLabel')}
 				isChecked={formSection.label?.showLabel}
+			/>
+			<SelectWithLabel
+				label={__('applies to')}
+				onChangeValue={onChangeValue('appliesTo')}
+				value={formSection.appliesTo}
+				options={APPLIES_TO_OPTIONS}
 			/>
 		</>
 	);
