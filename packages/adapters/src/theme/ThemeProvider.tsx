34d797e6c60363e65ed8d0e6f7b203ca7624b25b
Brent Christensen <brent@eventespresso.com>
2021-11-10 01:59:33 -0800

    add color mode props & force light mode (#1086)

--- packages/adapters/src/theme/ThemeProvider.tsx
+++ packages/adapters/src/theme/ThemeProvider.tsx
@@ -1,15 +1,20 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, LightMode } from '@chakra-ui/react';
 
 import { isRTL } from '@eventespresso/i18n';
 
-const theme = extendTheme({ styles: null, direction: isRTL() ? 'rtl' : 'ltr' });
+const theme = extendTheme({
+	direction: isRTL() ? 'rtl' : 'ltr',
+	initialColorMode: 'light',
+	styles: null,
+	useSystemColorMode: false,
+});
 
 const environment = { window, document };
 
 const ThemeProvider: React.FC = ({ children }) => {
 	return (
 		<ChakraProvider theme={theme} resetCSS environment={environment}>
-			{children}
+			<LightMode>{children}</LightMode>
 		</ChakraProvider>
 	);
 };
