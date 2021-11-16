229fbb74ec662caa431a9f599d95a9b163c2b2a6
Marlon Hario <hariomarlon83@gmail.com>
2021-11-17 02:35:42 +0800

    Solve conflict for event data E2E test for pagination overview.

--- packages/e2e-tests/specs/admin/events/overview/event-list/pagination.test.ts
+++ packages/e2e-tests/specs/admin/events/overview/event-list/pagination.test.ts
@@ -16,16 +16,43 @@ describe('Test overview pagination', () => {
 		// get the number and elements of rows availble
 		const count = await eventsListSurfer.getItemCount();
 
-		if (count) {
+		// check if there is no event in view all event if true create events
+		if (!count) {
+			// create events for testing
 			for (const args of eventData.upcomingNextMonth) {
 				await createNewEvent(args);
 			}
 			await Goto.eventsListPage();
 		}
 
-		// 1. count again the available event
-		// 2. set screen option pagination event
-		// 3. paginate until the end of the pagination then assert the number of rows base on pagination event option
-		expect(true).toBeTruthy();
+		// count all events
+		const countAllEventsList = await eventsListSurfer.getViewCount('View All Events');
+		// click screen option to get the pagination set
+		await page.click('#show-settings-link');
+		// get the default per page
+		const paginationEvents = await (await page.$('#espresso_events_default_per_page')).getAttribute('value');
+		// get how many set of page in pagination
+		const totalPages = await (await page.$('span.total-pages')).innerText();
+		// get the remainder countAllEventsList modulus to default per page
+		const getPageRemainder = countAllEventsList % Number(paginationEvents);
+		// this will get the number of event in the last pagiantion (this will use for assertion)
+		let countLastPageRows = 0;
+
+		// loop the pagination per page
+		for (let pages = 1; pages < Number(totalPages); pages++) {
+			// first get the link for next page in pagination
+			const href = await (await page.$('.pagination-links a.next-page')).getAttribute('href');
+			// go to next page link
+			await Promise.all([page.waitForLoadState(), page.goto(href)]);
+			// count event every paginate page
+			const countRowsPerPage = await eventsListSurfer.getItemCount();
+			// get the remainder per page by countRowsPerPage modulus to default per page
+			countLastPageRows = countRowsPerPage % Number(paginationEvents);
+			// assert event count every page pagination
+			expect(countRowsPerPage).toBeGreaterThanOrEqual(countLastPageRows);
+		}
+
+		// assert the getPageRemainder and countLastPageRows this will always be equal
+		expect(getPageRemainder).toBe(countLastPageRows);
 	});
 });
